3794f0eb60f629582fedfa87fdca39d54ee416e9
bill <bill.mill@gmail.com>
2012-03-03 11:00:21 -0500

    move runandprint to pub, rename to run

--- pub/pub.py
+++ pub/pub.py
@@ -1,3 +1,13 @@
+import envoy
+
 def task(f):
     f.__pub_task__ = True
     return f
+
+def run(cmd, *args, **kwargs):
+    out = run(cmd, *args, **kwargs)
+    if out.status_code > 0:
+        stdout.write("error running command: %s" % cmd)
+        exit(out.status_code)
+
+    stdout.write(out.std_out)
