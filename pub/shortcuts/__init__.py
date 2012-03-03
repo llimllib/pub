c9f6b228f91cf5236456de75dbc97d15065a0b58
bill <bill.mill@gmail.com>
2012-03-02 23:46:08 -0500

    add pip to shortcuts

--- pub/shortcuts/__init__.py
+++ pub/shortcuts/__init__.py
@@ -1,6 +1,6 @@
 from envoy import run
 
-for cmd in ["python", "ls", "rm"]:
+for cmd in ["python", "pip", "ls", "rm"]:
     exec """
 def %s(cmd, *args, **kwargs):
     return run("%s %%s" %% cmd, *args, **kwargs)""" % (cmd, cmd)
