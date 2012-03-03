efb253901deb49c7b843b7b88ace6314b49d98b7
bill <bill.mill@gmail.com>
2012-03-03 00:00:27 -0500

    print stdout instead of swallowing it

--- pub/shortcuts/__init__.py
+++ pub/shortcuts/__init__.py
@@ -1,6 +1,16 @@
 from envoy import run
+from sys import exit, stdout
+
+def runandprint(cmd, *args, **kwargs):
+    out = run(cmd, *args, **kwargs)
+
+    if out.status_code > 0:
+        stdout.write("error in command: '%s'\n" % cmd)
+        exit(127)
+
+    stdout.write(out.std_out)
 
 for cmd in ["python", "pip", "ls", "rm"]:
     exec """
 def %s(cmd, *args, **kwargs):
-    return run("%s %%s" %% cmd, *args, **kwargs)""" % (cmd, cmd)
+    return runandprint("%s %%s" %% cmd, *args, **kwargs)""" % (cmd, cmd)
