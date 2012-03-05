ffaa7039a5ac1b74d4cd54628de9d8e8dd123d44
bill <bill.mill@gmail.com>
2012-03-04 19:24:37 -0500

    add a nosetests shortcut

--- pub/shortcuts/__init__.py
+++ pub/shortcuts/__init__.py
@@ -1,6 +1,6 @@
 from pub import run
 
-for cmd in ["python", "pip", "ls", "rm"]:
+for cmd in ["python", "pip", "ls", "rm", "nosetests"]:
     exec """
 def %s(cmd, *args, **kwargs):
     return run("%s %%s" %% cmd, *args, **kwargs)""" % (cmd, cmd)
