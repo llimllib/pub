3bb518bae35542007092780cabff43487afa20cb
bill <bill.mill@gmail.com>
2012-03-09 12:05:09 -0500

    improve the task decorator

--- pub/pub.py
+++ pub/pub.py
@@ -5,6 +5,50 @@ from os import getcwdu
 from imp import load_source
 from os.path import abspath, join, isfile
 
+from networkx import DiGraph, simple_cycles
+
+def make_dependency_graph(tasks):
+    dep_graph = DiGraph()
+
+    #build the dependency graph in two steps. First add the nodes
+    for task in tasks:
+        assert not dep_graph.has_node(task), "Cannot add duplicate task: %s" % task
+        dep_graph.add_node(task)
+
+    #then add the edges.
+    taskdeps = [(t, t.__pub_dependencies__) for t in tasks if hasattr(t, "__pub_dependencies__")]
+    for task, deps in taskdeps:
+        for dep in deps:
+            assert not dep_graph.has_edge(task, dep), "Cannot add duplicate edge: %s, %s" % (task, dep)
+            dep_graph.add_edge(task, dep)
+
+    import pdb; pdb.set_trace()
+    if simple_cycles(dep_graph):
+        print "Cycle in the dependency graph: %s" % dep_graph
+        exit(127)
+
+    return dep_graph
+
+def get_tasks(tasks, dep_graph):
+    task_order = DiGraph()
+
+    for task in tasks:
+        if task_order.has_node(task): continue
+        task_order.add_node(task)
+
+        def get_deps(task, graph):
+            for t in graph.neighbors(task):
+                if task_order.has_node(t): continue
+
+                task_order.add_node(task)
+                task_order.add_edge(task, t)
+
+                get_deps(t, graph)
+
+        get_deps(task, dep_graph)
+
+    return reversed(topological_sort(task_order))
+
 def task(*args, **kwargs):
     def task_decorator(f):
         f.__pub_task__ = True
@@ -13,14 +57,15 @@ def task(*args, **kwargs):
 
     #If @task is not followed by parens, the function will be the first argument
     if type(args[0]) == type(lambda: 1):
-        return task_decorator(args[0])
+        #There are no dependencies, so reset the args list
+        f = args[0]
+        args = tuple()
+        return task_decorator(f)
 
     #otherwise, return the decorator function, which will be called with the decorated
     return task_decorator
 
 def main(options):
-    tasks = []
-
     if not isfile(options.pubfile):
         print "Unable to find pubfile %s" % abspath(options.pubfile)
         exit(127)
@@ -28,6 +73,7 @@ def main(options):
     try:
         #prevent python from writing pubfilec files.
         sys.dont_write_bytecode = True
+        #TODO: scan an array of pubfile name options
         pf = load_source("pubfile", options.pubfile)
         sys.dont_write_bytecode = False
     except:
@@ -45,13 +91,18 @@ def main(options):
         exit()
 
     if not options.tasks:
+        #TODO: handle default tasks
         print "no tasks specified. exiting"
         exit(127)
 
     unknown_tasks = [t for t in options.tasks if t not in tasks.keys()]
     if any(unknown_tasks):
         print "sorry, don't know how to perform tasks %s" % unknown_tasks
-        exit()
+        exit(127)
 
-    for task in options.tasks:
-        tasks[task]()
+    dep_graph = make_dependency_graph(tasks)
+
+    task_list = get_tasks(tasks)
+
+    for task in task_list:
+        task()
