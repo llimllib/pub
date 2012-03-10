b8335367f4d2de4d4b5d192511b71dd2eeb2ab80
bill <bill.mill@gmail.com>
2012-03-09 19:43:35 -0500

    lots of dep testing

--- test/test_deps.py
+++ test/test_deps.py
@@ -2,11 +2,57 @@ import sys
 
 import pub
 
+def make_task(*deps):
+    if not deps:
+        @pub.task
+        def _(): pass
+    else:
+        @pub.task(*deps)
+        def _(): pass
+    return _
+
 def test_make_dependency_graph():
-    foo = lambda x: sys.stdout.write("foo")
-    foo = pub.task(foo)
-    tasks = [foo]
+    tasks = {"foo": make_task()}
     dep_graph = pub.make_dependency_graph(tasks)
 
     assert len(dep_graph) == 1
-    assert foo == dep_graph.nodes()[0]
+    assert ["foo"] == dep_graph.nodes()
+    assert not dep_graph.edges()
+
+def test_simple_dep_graph():
+    tasks = {
+        "foo": make_task(),
+        "bar": make_task("foo")
+    }
+    dep_graph = pub.make_dependency_graph(tasks)
+
+    assert len(dep_graph) == 2
+    assert all(task in dep_graph for task in ["foo", "bar"])
+    assert [('bar', 'foo')] == dep_graph.edges()
+
+def test_bigger_dep_graph():
+    tasks = {
+        "foo": make_task(),
+        "bar": make_task("foo"),
+        "baz": make_task("bar", "foo"),
+    }
+
+    dep_graph = pub.make_dependency_graph(tasks)
+
+    assert len(dep_graph) == 3
+    assert all(task in dep_graph for task in ["foo", "bar", "baz"])
+    for edge in [('bar', 'foo'), ('baz', 'foo'), ('baz', 'bar')]:
+        assert edge in dep_graph.edges()
+
+def test_dep_graph_cycle():
+    tasks = {
+        "foo": make_task("baz"),
+        "bar": make_task("foo"),
+        "baz": make_task("bar"),
+    }
+
+    try:
+        pub.make_dependency_graph(tasks)
+        assert 1/0, "Task cycle failed to generate exception"
+    except pub.DependencyCycle, err:
+        assert "Cycle" in err.message
