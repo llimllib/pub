adda1718eefa2a721eaa05982f2693c8b608caa8
bill <bill.mill@gmail.com>
2012-03-10 00:52:31 -0500

    add a few more shortcuts... need to organize these

--- pub/shortcuts/__init__.py
+++ pub/shortcuts/__init__.py
@@ -1,4 +1,4 @@
 from shortcuts import make_shortcut, newer
 
-for cmd in ["python", "pip", "ls", "rm", "nosetests"]:
+for cmd in ["python", "pip", "ls", "rm", "nosetests", "mkdir", "rsync"]:
     globals()[cmd] = make_shortcut(cmd)
