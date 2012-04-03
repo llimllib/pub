eb9e736ad6742f131e02375d647b95abf43a5a87
bill <bill.mill@gmail.com>
2012-04-03 10:50:00 -0400

    push version to 0.0.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ packages = ['pub', 'pub.shortcuts']
 
 setup(
     name='pub',
-    version='0.0.3',
+    version='0.0.4',
     description='Python Utility Belt',
     long_description='TODO',
     author='Bill Mill',
